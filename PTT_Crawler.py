74c0e95170e5251bc58d30eb6d773f5e76a21738
Jian-Lin Jiang <kobe24684@gmail.com>
2021-06-14 19:50:50 +0800

    Refactor  function

--- PTT_Crawler.py
+++ PTT_Crawler.py
@@ -4,26 +4,8 @@ import re
 from urllib.request import urlretrieve   #模組的檔案 request , class(工廠)方便使用資料型態 > 
 import ssl 
 ssl._create_default_https_context = ssl._create_unverified_context   #(備註：加了之後mac資料才跑出來）
-# url = 'https://www.ptt.cc/bbs/Grad-ProbAsk/index.html'
-url = "https://www.ptt.cc/bbs/Beauty/index.html"
-reg_imgur_file  = re.compile('http[s]?://[i.]*imgur.com/\w+\.(?:jpg|png|gif)')
 
-for round in range(1):
-    res = requests.get(url, headers={
-        "cookie":"over18=1"
-    })
-    ##把抓到的res 告訴 BeautifulSoup 是html
-    soup = BeautifulSoup(res.text,'html.parser')   ## "lxml == html.parser"
-    ##抓取所有文章連結
-    articles = soup.select('div.title a')
-    # print(articles)
-    ##選擇: [最舊, 上頁, 下頁, 最新
-    paging = soup.select('div.btn-group-paging a')
-    ##做成下頁的連結網址
-    next_url = 'https://www.ptt.cc'+paging[1]['href'] 
-    ##不然會抓到First Page
-    url = next_url    
-    ##text兩個[]文字,文章超連結
+def download_images(articles):
     for article in articles:
         print(article.text,article['href'])
         res = requests.get('https://www.ptt.cc'+article['href'], headers={ 
@@ -36,3 +18,28 @@ for round in range(1):
             ID = re.search('http[s]?://[i.]*imgur.com/(\w+\.(?:jpg|png|gif))',image).group(1)  #\w+\.(? 亂碼
             print(ID)
             urlretrieve(image,ID)
+
+def crawler():
+    url = "https://www.ptt.cc/bbs/Beauty/index.html"
+    
+
+    for round in range(1):
+        res = requests.get(url, headers={
+            "cookie":"over18=1"
+        })
+        ##把抓到的res 告訴 BeautifulSoup 是html
+        soup = BeautifulSoup(res.text,'html.parser')   ## "lxml == html.parser"
+        ##抓取所有文章連結
+        articles = soup.select('div.title a')
+        # print(articles)
+        ##選擇: [最舊, 上頁, 下頁, 最新
+        paging = soup.select('div.btn-group-paging a')
+        ##做成下頁的連結網址
+        next_url = 'https://www.ptt.cc'+paging[1]['href'] 
+        ##不然會抓到First Page
+        url = next_url    
+        ##text兩個[]文字,文章超連結
+        download_images(articles)
+        
+reg_imgur_file  = re.compile('http[s]?://[i.]*imgur.com/\w+\.(?:jpg|png|gif)')
+crawler()
