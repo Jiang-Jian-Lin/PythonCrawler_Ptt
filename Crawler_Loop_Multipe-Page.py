6ea2d1e175f930749d330b45748e50284250244f
Jian-Lin Jiang <kobe24684@gmail.com>
2021-06-14 17:50:16 +0800

    Grab imgur url in articles

--- Crawler_Loop_Multipe-Page.py
+++ Crawler_Loop_Multipe-Page.py
@@ -1,8 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
-url = 'https://www.ptt.cc/bbs/Beauty/index.html'
-       
+# url = 'https://www.ptt.cc/bbs/Grad-ProbAsk/index.html'
+url = "https://www.ptt.cc/bbs/Beauty/index.html"
+
+reg_imgur_file  = re.compile('http[s]?://[i.]*imgur.com/\w+\.(?:jpg|png|gif)')
+
 for round in range(2):
     res = requests.get(url, headers={
         "cookie":"over18=1"
@@ -11,6 +15,7 @@ for round in range(2):
     soup = BeautifulSoup(res.text,'html.parser')   ## "lxml == html.parser"
     ##抓取所有文章連結
     articles = soup.select('div.title a')
+    # print(articles)
     ##選擇: [最舊, 上頁, 下頁, 最新
     paging = soup.select('div.btn-group-paging a')
     ##做成下頁的連結網址
@@ -20,5 +25,10 @@ for round in range(2):
     ##text兩個[]文字,文章超連結
     for article in articles:
         print(article.text,article['href'])
-
-
+        res = requests.get('https://www.ptt.cc'+article['href'], headers={ 
+        #request.get請求也要加上cookie，不然一樣會被18禁的cookie擋住
+        "cookie":"over18=1"
+    })  # 那篇文章所有HTML
+        images = reg_imgur_file.findall(res.text)
+        print(images)
+    
